66057ab35fb9001bb5de312edece9fb1cf820b5c
Aiham Meraish <lernoschool11@gmai.com lernoschool11-bit>
2026-04-18 23:09:27 +0300

    Configure seed in prisma.config.ts

--- backend/prisma.config.ts
+++ backend/prisma.config.ts
@@ -5,5 +5,8 @@ export default defineConfig({
   schema: "prisma/schema.prisma",
   datasource: {
     url: process.env.DATABASE_URL!
+  },
+  migrations: {
+    seed: "npx ts-node prisma/seed.ts"
   }
 })
